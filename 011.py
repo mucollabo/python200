1115ece829627a91a06152e90ac08a42e23db5a2
Charles An <mucollabo@gmail.com>
2020-06-09 16:43:38 +0900

    first commit

---
+++ 011.py
@@ -0,0 +1,3 @@
+scope = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5}
+for x in scope:
+    print(x, scope[x])
---
+++ 014.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 10:
+    x = x + 1
+    if x < 3:
+        continue
+    print(x)
+    if x > 7:
+        break
+        
